2f313e3cddf64c89201c657dd1bd9ee763145126
ArielMejia01 <arielgomezcr01@gmail.com>
2022-10-02 18:56:04 -0600

    Accediendo al admin de Django, nos permitira subir libros, imagenes y todo el modelo de la tabla

--- libreria/admin.py
+++ libreria/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+# la informacion que voy a utilizar esta en modelo
+from .models import Libro
 
 # Register your models here.
+# Registrar toda la informacion que necesito
+# Una vez incluido el Libro ya lo tenemos a disposicion en nuestro modelo
+# y tenemos que registrarlo en el administrador
+# ya cuando corramos el administrador ya tenga la manipulacion de ese libro
+admin.site.register(Libro)
