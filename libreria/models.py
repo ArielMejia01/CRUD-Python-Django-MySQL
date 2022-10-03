ecbf6b7f7a7eb4f37425b0699e8d3284bb1d6be7
ArielMejia01 <arielgomezcr01@gmail.com>
2022-10-02 18:24:47 -0600

    Modelo y migracion de la tabla libros a la Base de datos

--- libreria/models.py
+++ libreria/models.py
@@ -1,3 +1,12 @@
+from distutils.command.upload import upload
+from tabnanny import verbose
 from django.db import models
 
 # Create your models here.
+# Captura toda la estructura de nuestra tabla
+class Libro(models.Model):
+    id = models.AutoField(primary_key=True)
+    titulo = models.CharField(max_length=100, verbose_name='Título')
+    # Aqui se guardara la imagen que vamos a utilizar
+    imagen = models.ImageField(upload_to='imagenes/', verbose_name="Imagen" ,null=True)
+    description = models.TextField(verbose_name="Descripcion", null=True)
